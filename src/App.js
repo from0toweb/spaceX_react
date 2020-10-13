bcac1b40b0aef4eafbcb1e83ff62bdf23450ca37
Михаил Минченко <t9052585903@gmail.com>
2020-10-13 21:48:01 +0300

    add header and main section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import './style.css';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
 
 function App() {
-	return <div className="App" />;
+	return (
+		< >
+			<Header />
+			<Main />
+		</>
+	);
 }
 
 export default App;
