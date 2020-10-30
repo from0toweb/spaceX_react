bbb9e0a25214d3927015d934105c731925ac7a4b
Михаил Минченко <t9052585903@gmail.com>
2020-10-30 13:01:18 +0300

    Updates

---
+++ precache-manifest.934959106fbc01ea355320ac3a309fa5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34b20973ce3b1caae550df2a2ffad65b",
+    "url": "/spaceX_react/index.html"
+  },
+  {
+    "revision": "ea91b395598daafaa872",
+    "url": "/spaceX_react/static/css/main.5d8b7180.chunk.css"
+  },
+  {
+    "revision": "9dc9120e981fe86ef40d",
+    "url": "/spaceX_react/static/js/2.d11b2701.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/spaceX_react/static/js/2.d11b2701.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea91b395598daafaa872",
+    "url": "/spaceX_react/static/js/main.302b7868.chunk.js"
+  },
+  {
+    "revision": "fb72c7139dcc4b154142",
+    "url": "/spaceX_react/static/js/runtime-main.5e159ab2.js"
+  },
+  {
+    "revision": "d5ab31ce033fc418fd1af87f7b934e1b",
+    "url": "/spaceX_react/static/media/logo.d5ab31ce.svg"
+  }
+]);
